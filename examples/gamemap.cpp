cc83d4fbf784e5c9da3f8fcd0e621afdbf71ee3f
Adam Nielsen <malvineous@shikadi.net>
2014-11-09 01:14:41 +1000

    gamemap: Handle transparency with 256-colour images when rendering maps to .png
    Now we can default to a VGA palette with colour 255 as transparent, but if a
    palette is given then the alpha value is inspected so that images with a
    transparent colour set will correctly be brought across to the .png image.

--- examples/gamemap.cpp
+++ examples/gamemap.cpp
@@ -190,52 +190,42 @@ void map2dToPng(gm::Map2DPtr map, const gm::TilesetCollectionPtr& allTilesets,
 	png::image<png::index_pixel> png(outWidth, outHeight);
 
 	bool useMask;
-	bool gotPal = false;
+	gg::PaletteTablePtr srcPal;
 	for (gm::TilesetCollection::const_iterator
 		i = allTilesets->begin(); i != allTilesets->end(); i++
 	) {
 		if (i->second->getCaps() & gg::Tileset::HasPalette) {
-			gg::PaletteTablePtr srcPal = i->second->getPalette();
-			png::palette pal(srcPal->size());
-			int j = 0;
-			//pal[ 0] = png::color(0xFF, 0x00, 0xFF); // transparent
-			for (gg::PaletteTable::iterator
-				i = srcPal->begin(); i != srcPal->end(); i++, j++
-			) {
-				pal[j] = png::color(i->red, i->green, i->blue);
-			}
-			png.set_palette(pal);
-			useMask = false; // not enough room in the palette for transparent entry
-			gotPal = true;
+			srcPal = i->second->getPalette();
 			break;
 		}
 	}
-	if (!gotPal) {
-		// standard EGA palette
-		png::palette pal(17);
-		pal[ 0] = png::color(0xFF, 0x00, 0xFF); // transparent
-		pal[ 1] = png::color(0x00, 0x00, 0x00);
-		pal[ 2] = png::color(0x00, 0x00, 0xAA);
-		pal[ 3] = png::color(0x00, 0xAA, 0x00);
-		pal[ 4] = png::color(0x00, 0xAA, 0xAA);
-		pal[ 5] = png::color(0xAA, 0x00, 0x00);
-		pal[ 6] = png::color(0xAA, 0x00, 0xAA);
-		pal[ 7] = png::color(0xAA, 0x55, 0x00);
-		pal[ 8] = png::color(0xAA, 0xAA, 0xAA);
-		pal[ 9] = png::color(0x55, 0x55, 0x55);
-		pal[10] = png::color(0x55, 0x55, 0xFF);
-		pal[11] = png::color(0x55, 0xFF, 0x55);
-		pal[12] = png::color(0x55, 0xFF, 0xFF);
-		pal[13] = png::color(0xFF, 0x55, 0x55);
-		pal[14] = png::color(0xFF, 0x55, 0xFF);
-		pal[15] = png::color(0xFF, 0xFF, 0x55);
-		pal[16] = png::color(0xFF, 0xFF, 0xFF);
-		png.set_palette(pal);
-		useMask = true;
-
+	if (!srcPal) {
+		srcPal = gg::createPalette_DefaultVGA();
+		// Force last colour to be transparent
+		srcPal->at(255).alpha = 0;
+	}
+	png::palette pal(srcPal->size());
+	int j = 0;
+	//pal[ 0] = png::color(0xFF, 0x00, 0xFF); // transparent
+	png::tRNS transparency;
+	for (gg::PaletteTable::iterator
+		i = srcPal->begin(); i != srcPal->end(); i++, j++
+	) {
+		pal[j] = png::color(i->red, i->green, i->blue);
+		if (i->alpha == 0) transparency.push_back(j);
+	}
+	png.set_palette(pal);
+	useMask = srcPal->size() < 255; // only mask if enough room in the palette
+	if (useMask) {
+		// Increment any transparent indices because we're going to insert a new
+		// colour for transparency
+		for (png::tRNS::iterator i = transparency.begin(); i != transparency.end(); i++) {
+			(*i)++;
+		}
 		// Make first colour transparent
-		png::tRNS transparency;
 		transparency.push_back(0);
+	}
+	if (transparency.size() > 0) {
 		png.set_tRNS(transparency);
 	}
 
