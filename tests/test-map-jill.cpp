ceb64c1832e86c641fdbc4f58eff84295e013b89
Adam Nielsen <malvineous@shikadi.net>
2015-05-24 09:22:10 +1000

    Add test code for Jill maps

--- tests/test-map-jill.cpp
+++ tests/test-map-jill.cpp
@@ -1,6 +1,6 @@
 /**
- * @file   test-map-xargon.cpp
- * @brief  Test code for Xargon maps.
+ * @file   test-map-jill.cpp
+ * @brief  Test code for Jill of the Jungle maps.
  *
  * Copyright (C) 2010-2015 Adam Nielsen <malvineous@shikadi.net>
  *
@@ -20,45 +20,46 @@
 
 #include "test-map2d.hpp"
 
-class test_suppx1_map_xargon: public test_map2d
+// JILL.DMA
+class test_suppx1_map_jill: public test_map2d
 {
 	public:
-		test_suppx1_map_xargon()
+		test_suppx1_map_jill()
 		{
-			this->type = "map-xargon.x1";
+			this->type = "map-jill.x1";
 			this->written = false;
 		}
 
 		virtual std::string initialstate()
 		{
 			return STRING_WITH_NULLS(
-				"\x00\x00\x00\x00\x00\x00\x01" "0"
-				"\x01\x00\x00\x00\x00\x00\x03" "one"
-				"\x02\x00\x00\x00\x00\x00\x03" "two"
-				"\x03\x00\x00\x00\x00\x00\x05" "three"
-				"\x04\x00\x00\x00\x00\x00\x04" "four"
-				"\x05\x00\x00\x00\x00\x00\x04" "five"
-				"\x06\x00\x00\x00\x00\x00\x03" "six"
-				"\x07\x00\x00\x00\x00\x00\x05" "seven"
-				"\x08\x00\x00\x00\x00\x00\x05" "eight"
+				"\x00\x00" "\x00" "\x00" "\x00\x00" "\x01" "0"
+				"\x01\x00" "\x00" "\x00" "\x00\x00" "\x03" "one"
+				"\x02\x00" "\x00" "\x00" "\x00\x00" "\x03" "two"
+				"\x03\x00" "\x00" "\x00" "\x00\x00" "\x05" "three"
+				"\x04\x00" "\x00" "\x00" "\x00\x00" "\x04" "four"
+				"\x05\x00" "\x00" "\x00" "\x00\x00" "\x04" "five"
+				"\x06\x00" "\x00" "\x00" "\x00\x00" "\x03" "six"
+				"\x07\x00" "\x00" "\x00" "\x00\x00" "\x05" "seven"
+				"\x08\x00" "\x00" "\x00" "\x00\x00" "\x05" "eight"
 			);
 		}
 };
 
-class test_map_xargon: public test_map2d
+class test_map_jill: public test_map2d
 {
 	public:
-		test_map_xargon()
+		test_map_jill()
 		{
-			this->type = "map-xargon";
+			this->type = "map-jill";
 			this->pxWidth = 128 * 16;
 			this->pxHeight = 64 * 16;
 			this->numLayers = 2;
 			this->mapCode[0].code = 0x01;
 			this->mapCode[1].code = 0x01;
-			this->suppResult[SuppItem::Extra1].reset(new test_suppx1_map_xargon());
+			this->suppResult[SuppItem::Extra1].reset(new test_suppx1_map_jill());
 			this->skipInstDetect.push_back("map-wordresc");
-			this->skipInstDetect.push_back("map-jill");
+			this->skipInstDetect.push_back("map-xargon");
 		}
 
 		void addTests()
@@ -115,9 +116,9 @@ class test_map_xargon: public test_map2d
 					"\x00\x00" "\x00\x00"
 					"\x00\x00" "\x00\x00" "\x00\x00" "\x00\x00\x00\x00"
 					"\x00\x00" "\x00\x00"
-				) + std::string(97, '\x00') // empty savedata
+				) + std::string(70, '\x00') // empty savedata
 			;
 		}
 };
 
-IMPLEMENT_TESTS(map_xargon);
+IMPLEMENT_TESTS(map_jill);
