c79a765eb98b232d52133a9e2a3829546d3d72f0
Adam Nielsen <malvineous@shikadi.net>
2015-06-11 22:37:33 +1000

    C++11: Update example 'hello world' program

--- examples/hello.cpp
+++ examples/hello.cpp
@@ -1,4 +1,5 @@
 #include <camoto/stream_file.hpp>
+#include <camoto/util.hpp>
 #include <camoto/gamemaps.hpp>
 #include <iostream>
 
@@ -7,15 +8,12 @@ using namespace camoto::gamemaps;
 
 int main(void)
 {
-	// Get hold of the Manager class
-	ManagerPtr manager = getManager();
-
 	// Use the manager to look up a particular map format
-	MapTypePtr mapType = manager->getMapTypeByCode("map-xargon");
+	auto mapType = MapManager::byCode("map-xargon");
 
-	// Open a map file on disk
-	stream::file_sptr file(new stream::file());
-	file->open("board_01.xr1");
+	// Open a map file on disk (false means don't create the file if it doesn't
+	// exist)
+	auto file = std::make_unique<stream::file>("board_01.xr1", false);
 
 	// We cheat here - we should check and load any supplementary files, but
 	// for the sake of keeping this example simple we know this format doesn't
@@ -23,20 +21,20 @@ int main(void)
 	camoto::SuppData supps;
 
 	// Use the map format handler to read in the file we opened as a map
-	MapPtr map;
+	std::shared_ptr<Map> map;
 	try {
-		map = mapType->open(file, supps);
+		map = mapType->open(std::move(file), supps);
 	} catch (const stream::open_error& e) {
 		std::cerr << "Error opening map: " << e.what() << std::endl;
 		return 1;
 	}
 
 	// See if the map is a 2D grid-based one
-	Map2DPtr map2d = boost::dynamic_pointer_cast<Map2D>(map);
+	auto map2d = std::dynamic_pointer_cast<Map2D>(map);
 	if (map2d) {
 
 		// It is, print the number of layers
-		std::cout << "This map has " << map2d->getLayerCount() << " layers."
+		std::cout << "This map has " << map2d->layers().size() << " layers."
 			<< std::endl;
 
 	} else {
