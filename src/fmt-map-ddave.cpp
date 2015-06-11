61c4dfdb07c345dbc8660e3e893f85ef222e66fb
Adam Nielsen <malvineous@shikadi.net>
2015-06-11 22:43:46 +1000

    C++11: Update Dangerous Dave format handler

--- src/fmt-map-ddave.cpp
+++ src/fmt-map-ddave.cpp
@@ -22,7 +22,9 @@
  */
 
 #include <camoto/iostream_helpers.hpp>
-#include "map2d-generic.hpp"
+#include <camoto/util.hpp> // make_unique
+#include "map-core.hpp"
+#include "map2d-core.hpp"
 #include "fmt-map-ddave.hpp"
 
 #define DD_MAP_WIDTH            100
@@ -51,99 +53,264 @@ namespace gamemaps {
 
 using namespace camoto::gamegraphics;
 
-
-class Layer_DDaveBackground: virtual public GenericMap2D::Layer
+class Layer_DDave_Background: virtual public Map2DCore::LayerCore
 {
 	public:
-		Layer_DDaveBackground(ItemPtrVectorPtr& items, ItemPtrVectorPtr& validItems)
-			:	GenericMap2D::Layer(
-					"Background",
-					Map2D::Layer::NoCaps,
-					0, 0,
-					0, 0,
-					items, validItems
-				)
+		Layer_DDave_Background(stream::input& content)
 		{
+			// Read the background layer
+			uint8_t bg[DD_LAYER_LEN_BG];
+			content.read((char *)bg, DD_LAYER_LEN_BG);
+
+			this->v_allItems.reserve(DD_MAP_WIDTH * DD_MAP_HEIGHT);
+			for (unsigned int i = 0; i < DD_LAYER_LEN_BG; i++) {
+				Map2D::Layer::Item t;
+				t.type = Map2D::Layer::Item::Type::Default;
+				t.pos = {i % DD_MAP_WIDTH, i / DD_MAP_WIDTH};
+				t.code = bg[i];
+				if (t.code != DD_DEFAULT_BGTILE) this->v_allItems.push_back(t);
+			}
 		}
 
-		virtual Map2D::Layer::ImageType imageFromCode(
-			const Map2D::Layer::ItemPtr& item, const TilesetCollectionPtr& tileset,
-			ImagePtr *out) const
+		virtual std::string title() const
 		{
-			TilesetCollection::const_iterator t = tileset->find(BackgroundTileset1);
-			if (t == tileset->end()) return Map2D::Layer::Unknown; // no tileset?!
+			return "Background";
+		}
 
-			const Tileset::VC_ENTRYPTR& images = t->second->getItems();
-			if (item->code >= images.size()) return Map2D::Layer::Unknown; // out of range
-			*out = t->second->openImage(images[item->code]);
-			return Map2D::Layer::Supplied;
+		virtual Caps caps() const
+		{
+			return Caps::Default;
+		}
+
+		virtual Map2D::Layer::ImageFromCodeInfo imageFromCode(
+			const Map2D::Layer::Item& item, const TilesetCollection& tileset)
+			const
+		{
+			ImageFromCodeInfo ret;
+
+			auto t = tileset.find(ImagePurpose::BackgroundTileset1);
+			if (t == tileset.end()) { // no tileset?!
+				ret.type = ImageFromCodeInfo::ImageType::Unknown;
+			} else {
+				auto& images = t->second->files();
+				if (item.code >= images.size()) { // out of range
+					ret.type = ImageFromCodeInfo::ImageType::Unknown;
+				} else {
+					ret.img = t->second->openImage(images[item.code]);
+					ret.type = ImageFromCodeInfo::ImageType::Supplied;
+				}
+			}
+			return ret;
+		}
+
+		virtual std::vector<Item> availableItems() const
+		{
+			std::vector<Item> items;
+			for (unsigned int i = 0; i <= DD_MAX_VALID_TILECODE; i++) {
+				// The default tile actually has an image, so don't exclude it
+				if (i == DD_DEFAULT_BGTILE) continue;
+
+				Map2D::Layer::Item t;
+				t.type = Map2D::Layer::Item::Type::Default;
+				t.pos = {0, 0};
+				t.code = i;
+				items.push_back(t);
+			}
+			return items;
 		}
 };
 
-
-class Map2D_DDave: virtual public GenericMap2D
+class Map_DDave: public MapCore, public Map2DCore
 {
 	public:
-		Map2D_DDave(LayerPtrVector& layers, PathPtrVectorPtr& paths)
-			:	GenericMap2D(
-					Map::Attributes(), Map::GraphicsFilenames(),
-					Map2D::HasViewport | Map2D::HasPaths | Map2D::FixedPathCount,
-					20 * DD_TILE_WIDTH, 10 * DD_TILE_HEIGHT, // viewport size
-					DD_MAP_WIDTH, DD_MAP_HEIGHT,
-					DD_TILE_WIDTH, DD_TILE_HEIGHT,
-					layers, paths
-				)
+		Map_DDave(std::unique_ptr<stream::inout> content)
+			:	content(std::move(content))
+		{
+			this->content->seekg(0, stream::start);
+
+			// Read the path
+			uint8_t pathdata[DD_LAYER_LEN_PATH];
+			this->content->read((char *)pathdata, DD_LAYER_LEN_PATH);
+			auto path = std::make_shared<Map2D::Path>();
+			path->fixed = true;
+			path->forceClosed = false;
+			path->maxPoints = 128;
+			Point next{0, 0};
+			for (unsigned int i = 0; i < DD_LAYER_LEN_PATH; i += 2) {
+				if ((pathdata[i] == DD_PATH_END) && (pathdata[i+1] == DD_PATH_END)) break; // end of path
+				next.x += (int8_t)pathdata[i];
+				next.y += (int8_t)pathdata[i + 1];
+				path->points.push_back(next);
+			}
+			// Hard-code the starting point and copies of each path
+			unsigned int level = 3;
+			switch (level) {
+				case 3:
+					path->start.push_back(Point{44 * DD_TILE_WIDTH, 4 * DD_TILE_HEIGHT});
+					path->start.push_back(Point{59 * DD_TILE_WIDTH, 4 * DD_TILE_HEIGHT});
+					break;
+			}
+			this->v_paths.push_back(path);
+
+			// Read the background layer
+			this->v_layers.push_back(
+				std::make_shared<Layer_DDave_Background>(*this->content)
+			);
+		}
+
+		virtual ~Map_DDave()
 		{
 		}
 
-		Map2D::ImageAttachment getBackgroundImage(
-			const TilesetCollectionPtr& tileset, ImagePtr *outImage,
-			PaletteEntry *outColour) const
+		virtual std::map<ImagePurpose, GraphicsFilename> graphicsFilenames() const
 		{
-			outColour->red = 0;
-			outColour->green = 0;
-			outColour->blue = 0;
-			outColour->alpha = 255;
-			return Map2D::SingleColour;
+			return {
+				std::make_pair(
+					ImagePurpose::BackgroundTileset1,
+					GraphicsFilename{"vgadave.dav", "tls-ddave-vga"}
+				),
+			};
 		}
+
+		virtual void flush()
+		{
+			assert(this->layers().size() == 1);
+			assert(this->paths().size() == 1);
+
+			this->content->truncate(DD_LAYER_LEN_PATH + DD_LAYER_LEN_BG + DD_PAD_LEN);
+			this->content->seekp(0, stream::start);
+
+			// Write the path
+			uint8_t path[DD_LAYER_LEN_PATH];
+			memset(path, 0, DD_LAYER_LEN_PATH);
+			auto& first_path = this->paths().at(0);
+			unsigned int pathpos = 0;
+			unsigned int lastX = 0, lastY = 0;
+			int8_t x = 0, y = 0;
+			for (const auto& i : first_path->points) {
+				if (pathpos > 256) throw stream::error("Path too long (max 128 segments)");
+
+				// Convert from relative to (0,0), to relative to previous point
+				// Have to cast these to int8_t first so they're 8-bit but the sign is kept
+				// intact (-1 still is -1) then to uint8_t so -1 becomes 255.
+				x = i.x - lastX;
+				y = i.y - lastY;
+				lastX = i.x;
+				lastY = i.y;
+
+				if (((uint8_t)x == DD_PATH_END) && ((uint8_t)y == DD_PATH_END)) {
+					// Can't write these magic values, so tweak the data slightly
+					lastY++;
+					y++;
+					// This should work fine unless this is the last point in the path, but
+					// the condition below will catch that.
+				}
+				path[pathpos++] = (uint8_t)x;
+				path[pathpos++] = (uint8_t)y;
+			}
+			if (((uint8_t)x == DD_PATH_END) && ((uint8_t)y == DD_PATH_END)) {
+				throw stream::error("The last point in the path happens to have a "
+					"special magic offset that cannot be saved in a Dangerous Dave map.  "
+					"Please move the last or second last point by at least one pixel.");
+			}
+
+			// Add terminator if there's enough room.
+#warning TODO: Test to see if this is correct, or if a terminator is always required
+			if (pathpos < 256) {
+				path[pathpos++] = DD_PATH_END;
+				path[pathpos++] = DD_PATH_END;
+			}
+			this->content->write((char *)path, DD_LAYER_LEN_PATH);
+
+			// Write the background layer
+			uint8_t bg[DD_LAYER_LEN_BG];
+			memset(bg, DD_DEFAULT_BGTILE, DD_LAYER_LEN_BG); // default background tile
+			auto layer = this->layers().at(0);
+			for (auto& i : layer->items()) {
+				if ((i.pos.x > DD_MAP_WIDTH) || (i.pos.y > DD_MAP_HEIGHT)) {
+					throw stream::error("Layer has tiles outside map boundary!");
+				}
+				bg[i.pos.y * DD_MAP_WIDTH + i.pos.x] = i.code;
+			}
+			this->content->write((char *)bg, DD_LAYER_LEN_BG);
+
+			// Write out padding
+			uint8_t pad[DD_PAD_LEN];
+			memset(pad, 0, DD_PAD_LEN);
+			this->content->write((char *)pad, DD_PAD_LEN);
+
+			this->content->flush();
+			return;
+		}
+
+		virtual Caps caps() const
+		{
+			return
+				Map2D::Caps::HasViewport
+				| Map2D::Caps::HasMapSize
+				| Map2D::Caps::HasTileSize
+			;
+		}
+
+		virtual Point viewport() const
+		{
+			return {20 * DD_TILE_WIDTH, 10 * DD_TILE_HEIGHT};
+		}
+
+		virtual Point mapSize() const
+		{
+			return {DD_MAP_WIDTH, DD_MAP_HEIGHT};
+		}
+
+		virtual Point tileSize() const
+		{
+			return {DD_TILE_WIDTH, DD_TILE_HEIGHT};
+		}
+
+		Background background(const TilesetCollection& tileset) const
+		{
+			Background bg;
+			bg.att = Background::Attachment::SingleColour;
+			bg.clr = gamegraphics::PaletteEntry{0, 0, 0, 255};
+			return bg;
+		}
+
+	private:
+		std::unique_ptr<stream::inout> content;
 };
 
 
-std::string MapType_DDave::getMapCode() const
+std::string MapType_DDave::code() const
 {
 	return "map-ddave";
 }
 
-std::string MapType_DDave::getFriendlyName() const
+std::string MapType_DDave::friendlyName() const
 {
 	return "Dangerous Dave level";
 }
 
-std::vector<std::string> MapType_DDave::getFileExtensions() const
+std::vector<std::string> MapType_DDave::fileExtensions() const
 {
-	std::vector<std::string> vcExtensions;
-	vcExtensions.push_back("dav");
-	return vcExtensions;
+	return {"dav"};
 }
 
-std::vector<std::string> MapType_DDave::getGameList() const
+std::vector<std::string> MapType_DDave::games() const
 {
-	std::vector<std::string> vcGames;
-	vcGames.push_back("Dangerous Dave");
-	return vcGames;
+	return {"Dangerous Dave"};
 }
 
-MapType::Certainty MapType_DDave::isInstance(stream::input_sptr psMap) const
+MapType::Certainty MapType_DDave::isInstance(stream::input& content) const
 {
-	stream::pos lenMap = psMap->size();
+	stream::pos lenMap = content.size();
 
 	// TESTED BY: fmt_map_ddave_isinstance_c01
 	if (lenMap != DD_FILESIZE) return MapType::DefinitelyNo; // wrong size
 
 	// Read in the layer and make sure all the tile codes are within range
 	uint8_t bg[DD_LAYER_LEN_BG];
-	psMap->seekg(DD_LAYER_OFF_BG, stream::start);
-	stream::len r = psMap->try_read(bg, DD_LAYER_LEN_BG);
+	content.seekg(DD_LAYER_OFF_BG, stream::start);
+	stream::len r = content.try_read(bg, DD_LAYER_LEN_BG);
 	if (r != DD_LAYER_LEN_BG) return MapType::DefinitelyNo; // read error
 	for (unsigned int i = 0; i < DD_LAYER_LEN_BG; i++) {
 		// TESTED BY: fmt_map_ddave_isinstance_c02
@@ -154,165 +321,23 @@ MapType::Certainty MapType_DDave::isInstance(stream::input_sptr psMap) const
 	return MapType::DefinitelyYes;
 }
 
-MapPtr MapType_DDave::create(SuppData& suppData) const
+std::unique_ptr<Map> MapType_DDave::create(
+	std::unique_ptr<stream::inout> content, SuppData& suppData) const
 {
 	// TODO: Implement
 	throw stream::error("Not implemented yet!");
 }
 
-MapPtr MapType_DDave::open(stream::input_sptr input, SuppData& suppData) const
+std::unique_ptr<Map> MapType_DDave::open(
+	std::unique_ptr<stream::inout> content, SuppData& suppData) const
 {
-	input->seekg(0, stream::start);
-
-	// Read the path
-	uint8_t pathdata[DD_LAYER_LEN_PATH];
-	input->read((char *)pathdata, DD_LAYER_LEN_PATH);
-	Map2D::PathPtrVectorPtr paths(new Map2D::PathPtrVector());
-	Map2D::PathPtr pathptr(new Map2D::Path());
-	pathptr->fixed = true;
-	pathptr->forceClosed = false;
-	pathptr->maxPoints = 128;
-	unsigned int nextX = 0, nextY = 0;
-	for (unsigned int i = 0; i < DD_LAYER_LEN_PATH; i += 2) {
-		if ((pathdata[i] == DD_PATH_END) && (pathdata[i+1] == DD_PATH_END)) break; // end of path
-		nextX += (int8_t)pathdata[i];
-		nextY += (int8_t)pathdata[i + 1];
-		pathptr->points.push_back(Map2D::Path::point(nextX, nextY));
-	}
-	// Hard-code the starting point and copies of each path
-	unsigned int level = 3;
-	switch (level) {
-		case 3:
-			pathptr->start.push_back(Map2D::Path::point(44 * DD_TILE_WIDTH, 4 * DD_TILE_HEIGHT));
-			pathptr->start.push_back(Map2D::Path::point(59 * DD_TILE_WIDTH, 4 * DD_TILE_HEIGHT));
-			break;
-	}
-	paths->push_back(pathptr);
-
-	// Read the background layer
-	uint8_t bg[DD_LAYER_LEN_BG];
-	input->read((char *)bg, DD_LAYER_LEN_BG);
-
-	Map2D::Layer::ItemPtrVectorPtr tiles(new Map2D::Layer::ItemPtrVector());
-	tiles->reserve(DD_MAP_WIDTH * DD_MAP_HEIGHT);
-	for (unsigned int i = 0; i < DD_LAYER_LEN_BG; i++) {
-		Map2D::Layer::ItemPtr t(new Map2D::Layer::Item());
-		t->type = Map2D::Layer::Item::Default;
-		t->x = i % DD_MAP_WIDTH;
-		t->y = i / DD_MAP_WIDTH;
-		t->code = bg[i];
-		if (t->code != DD_DEFAULT_BGTILE) tiles->push_back(t);
-	}
-
-	// Populate the list of permitted tiles
-	Map2D::Layer::ItemPtrVectorPtr validBGItems(new Map2D::Layer::ItemPtrVector());
-	for (unsigned int i = 0; i <= DD_MAX_VALID_TILECODE; i++) {
-		// The default tile actually has an image, so don't exclude it
-		if (i == DD_DEFAULT_BGTILE) continue;
-
-		Map2D::Layer::ItemPtr t(new Map2D::Layer::Item());
-		t->type = Map2D::Layer::Item::Default;
-		t->x = 0;
-		t->y = 0;
-		t->code = i;
-		validBGItems->push_back(t);
-	}
-
-	// Create the map structures
-	Map2D::LayerPtr bgLayer(new Layer_DDaveBackground(tiles, validBGItems));
-
-	Map2D::LayerPtrVector layers;
-	layers.push_back(bgLayer);
-
-	Map2DPtr map(new Map2D_DDave(layers, paths));
-	return map;
+	return std::make_unique<Map_DDave>(std::move(content));
 }
 
-void MapType_DDave::write(MapPtr map, stream::expanding_output_sptr output,
-	ExpandingSuppData& suppData) const
-{
-	Map2DPtr map2d = boost::dynamic_pointer_cast<Map2D>(map);
-	if (!map2d) throw stream::error("Cannot write this type of map as this format.");
-	if (map2d->getLayerCount() != 1)
-		throw stream::error("Incorrect layer count for this format.");
-
-	// Write the path
-	uint8_t path[DD_LAYER_LEN_PATH];
-	memset(path, 0, DD_LAYER_LEN_PATH);
-	Map2D::PathPtrVectorPtr paths = map2d->getPaths();
-	if (paths->size() != 1) throw stream::error("Incorrect path count for this format.");
-	Map2D::PathPtr first_path = paths->at(0);
-	unsigned int pathpos = 0;
-	unsigned int lastX = 0, lastY = 0;
-	int8_t x = 0, y = 0;
-	for (Map2D::Path::point_vector::const_iterator i = first_path->points.begin();
-		i != first_path->points.end();
-		i++
-	) {
-		if (pathpos > 256) throw stream::error("Path too long (max 128 segments)");
-
-		// Convert from relative to (0,0), to relative to previous point
-		// Have to cast these to int8_t first so they're 8-bit but the sign is kept
-		// intact (-1 still is -1) then to uint8_t so -1 becomes 255.
-		x = i->first - lastX;
-		y = i->second - lastY;
-		lastX = i->first;
-		lastY = i->second;
-
-		if (((uint8_t)x == DD_PATH_END) && ((uint8_t)y == DD_PATH_END)) {
-			// Can't write these magic values, so tweak the data slightly
-			lastY++;
-			y++;
-			// This should work fine unless this is the last point in the path, but
-			// the condition below will catch that.
-		}
-		path[pathpos++] = (uint8_t)x;
-		path[pathpos++] = (uint8_t)y;
-	}
-	if (((uint8_t)x == DD_PATH_END) && ((uint8_t)y == DD_PATH_END)) {
-		throw stream::error("The last point in the path happens to have a "
-			"special magic offset that cannot be saved in a Dangerous Dave map.  "
-			"Please move the last or second last point by at least one pixel.");
-	}
-
-	// Add terminator if there's enough room.
-	// TODO: Test to see if this is correct, or if a terminator is always required
-	if (pathpos < 256) {
-		path[pathpos++] = DD_PATH_END;
-		path[pathpos++] = DD_PATH_END;
-	}
-	output->write((char *)path, DD_LAYER_LEN_PATH);
-
-	// Write the background layer
-	uint8_t bg[DD_LAYER_LEN_BG];
-	memset(bg, DD_DEFAULT_BGTILE, DD_LAYER_LEN_BG); // default background tile
-	Map2D::LayerPtr layer = map2d->getLayer(0);
-	const Map2D::Layer::ItemPtrVectorPtr items = layer->getAllItems();
-	for (Map2D::Layer::ItemPtrVector::const_iterator i = items->begin();
-		i != items->end();
-		i++
-	) {
-		if (((*i)->x > DD_MAP_WIDTH) || ((*i)->y > DD_MAP_HEIGHT)) {
-			throw stream::error("Layer has tiles outside map boundary!");
-		}
-		bg[(*i)->y * DD_MAP_WIDTH + (*i)->x] = (*i)->code;
-	}
-	output->write((char *)bg, DD_LAYER_LEN_BG);
-
-	// Write out padding
-	uint8_t pad[DD_PAD_LEN];
-	memset(pad, 0, DD_PAD_LEN);
-	output->write((char *)pad, DD_PAD_LEN);
-
-	output->flush();
-	return;
-}
-
-SuppFilenames MapType_DDave::getRequiredSupps(stream::input_sptr input,
+SuppFilenames MapType_DDave::getRequiredSupps(stream::input& content,
 	const std::string& filename) const
 {
-	SuppFilenames supps;
-	return supps;
+	return {};
 }
 
 } // namespace gamemaps
