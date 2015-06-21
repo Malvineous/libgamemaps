fb3bb1cf135864e388566ae0268d319674f4d130
Adam Nielsen <malvineous@shikadi.net>
2015-06-21 22:20:01 +1000

    C++11: Update Captain Comic

--- src/fmt-map-ccomic.cpp
+++ src/fmt-map-ccomic.cpp
@@ -21,9 +21,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <boost/scoped_array.hpp>
 #include <camoto/iostream_helpers.hpp>
-#include "map2d-generic.hpp"
+#include <camoto/util.hpp> // make_unique
+#include "map-core.hpp"
+#include "map2d-core.hpp"
 #include "fmt-map-ccomic.hpp"
 
 #define CC_TILE_WIDTH           16
@@ -40,72 +41,226 @@ namespace gamemaps {
 
 using namespace camoto::gamegraphics;
 
-class Layer_CComicBackground: virtual public GenericMap2D::Layer
+class Layer_CComic_Background: public Map2DCore::LayerCore
 {
 	public:
-		Layer_CComicBackground(ItemPtrVectorPtr& items,
-			ItemPtrVectorPtr& validItems)
-			:	GenericMap2D::Layer(
-					"Background",
-					Map2D::Layer::NoCaps,
-					0, 0,
-					0, 0,
-					items, validItems
-				)
+		Layer_CComic_Background(stream::input& content, const Point& mapSize)
+		{
+			// Read the background layer
+			auto mapLen = mapSize.x * mapSize.y;
+			std::vector<uint8_t> bg;
+			bg.resize(mapLen);
+			content.read(bg.data(), mapLen);
+
+			this->v_allItems.reserve(mapLen);
+			for (unsigned int i = 0; i < mapLen; i++) {
+				if (bg[i] == CC_DEFAULT_BGTILE) continue;
+
+				Item t;
+				t.type = Item::Type::Default;
+				t.pos = {i % mapSize.x, i / mapSize.x};
+				t.code = bg[i];
+				this->v_allItems.push_back(t);
+			}
+		}
+
+		virtual ~Layer_CComic_Background()
 		{
 		}
 
-		virtual Map2D::Layer::ImageType imageFromCode(
-			const Map2D::Layer::ItemPtr& item, const TilesetCollectionPtr& tileset,
-			ImagePtr *out) const
+		void flush(stream::output& content, const Point& mapSize)
 		{
-			TilesetCollection::const_iterator t = tileset->find(BackgroundTileset1);
-			if (t == tileset->end()) return Map2D::Layer::Unknown; // no tileset?!
+			// Write the background layer
+			unsigned int mapLen = mapSize.y * mapSize.y;
 
-			const Tileset::VC_ENTRYPTR& images = t->second->getItems();
-			if (item->code >= images.size()) return Map2D::Layer::Unknown; // out of range
-			*out = t->second->openImage(images[item->code]);
-			return Map2D::Layer::Supplied;
+			std::vector<uint8_t> bg(mapLen, CC_DEFAULT_BGTILE);
+			for (auto& i : this->v_allItems) {
+				if ((i.pos.x >= mapSize.x) || (i.pos.y >= mapSize.y)) {
+					throw stream::error("Layer has tiles outside map boundary!");
+				}
+				bg[i.pos.y * mapSize.x + i.pos.x] = i.code;
+			}
+			assert(bg.size() == mapLen);
+			content.write(bg.data(), mapLen);
+			return;
 		}
 
+		virtual std::string title() const
+		{
+			return "Background";
+		}
+
+		virtual Caps caps() const
+		{
+			return Caps::Default;
+		}
+
+		virtual ImageFromCodeInfo imageFromCode(const Item& item,
+			const TilesetCollection& tileset) const
+		{
+			ImageFromCodeInfo ret;
+
+			auto t = tileset.find(ImagePurpose::BackgroundTileset1);
+			if (t == tileset.end()) { // no tileset?!
+				ret.type = ImageFromCodeInfo::ImageType::Unknown;
+				return ret;
+			}
+
+			auto& images = t->second->files();
+			if (item.code >= images.size()) { // out of range
+				ret.type = ImageFromCodeInfo::ImageType::Unknown;
+				return ret;
+			}
+
+			ret.img = t->second->openImage(images[item.code]);
+			ret.type = ImageFromCodeInfo::ImageType::Supplied;
+			return ret;
+		}
+
+		virtual std::vector<Item> availableItems() const
+		{
+			std::vector<Item> validItems;
+			for (unsigned int i = 0; i <= CC_MAX_VALID_TILECODE; i++) {
+				if (i == CC_DEFAULT_BGTILE) continue;
+
+				Item t;
+				t.type = Item::Type::Default;
+				t.pos = {0, 0};
+				t.code = i;
+				validItems.push_back(t);
+			}
+			return validItems;
+		}
 };
 
+class Map_CComic: public MapCore, public Map2DCore
+{
+	public:
+		Map_CComic(std::unique_ptr<stream::inout> content,
+			std::unique_ptr<stream::inout> exe)
+			:	content(std::move(content)),
+				exe(std::move(exe))
+		{
+			this->content->seekg(0, stream::start);
 
-std::string MapType_CComic::getMapCode() const
+			*this->content
+				>> u16le(this->mapSizeTiles.x)
+				>> u16le(this->mapSizeTiles.y)
+			;
+
+			this->exe->seekg(0, stream::start);
+			*this->exe >> nullPadded(this->tilesetFilename, 14);
+
+			// Read the background layer
+			this->v_layers.push_back(
+				std::make_shared<Layer_CComic_Background>(*this->content, this->mapSizeTiles)
+			);
+		}
+
+		virtual ~Map_CComic()
+		{
+		}
+
+		virtual void flush()
+		{
+			this->content->seekp(0, stream::start);
+			*this->content
+				<< u16le(this->mapSizeTiles.x)
+				<< u16le(this->mapSizeTiles.y)
+			;
+			auto layerBG = dynamic_cast<Layer_CComic_Background*>(this->v_layers[0].get());
+			layerBG->flush(*this->content, this->mapSizeTiles);
+			return;
+		}
+
+		virtual std::map<ImagePurpose, GraphicsFilename> graphicsFilenames() const
+		{
+			return {
+				std::make_pair(
+					ImagePurpose::BackgroundTileset1,
+					GraphicsFilename{this->tilesetFilename, "tls-ccomic"}
+				),
+			};
+		}
+
+		virtual Caps caps() const
+		{
+			return
+				Map2D::Caps::HasViewport
+				| Map2D::Caps::HasMapSize
+				| Map2D::Caps::HasTileSize
+			;
+		}
+
+		virtual Point viewport() const
+		{
+			return {193, 160};
+		}
+
+		virtual Point mapSize() const
+		{
+			return this->mapSizeTiles;
+		}
+
+		virtual Point tileSize() const
+		{
+			return {CC_TILE_WIDTH, CC_TILE_HEIGHT};
+		}
+
+		Background background(const TilesetCollection& tileset) const
+		{
+#warning Is the background really black, or does it use the first tile?
+			Background bg;
+			bg.att = Background::Attachment::SingleColour;
+			bg.clr = gamegraphics::PaletteEntry{0, 0, 0, 255};
+			return bg;
+		}
+
+	private:
+		std::unique_ptr<stream::inout> content;
+		std::unique_ptr<stream::inout> exe;
+		Point mapSizeTiles;
+		std::string tilesetFilename;
+};
+
+std::string MapType_CComic::code() const
 {
 	return "map-ccomic";
 }
 
-std::string MapType_CComic::getFriendlyName() const
+std::string MapType_CComic::friendlyName() const
 {
 	return "Captain Comic level";
 }
 
-std::vector<std::string> MapType_CComic::getFileExtensions() const
+std::vector<std::string> MapType_CComic::fileExtensions() const
 {
 	std::vector<std::string> vcExtensions;
 	vcExtensions.push_back("pt");
 	return vcExtensions;
 }
 
-std::vector<std::string> MapType_CComic::getGameList() const
+std::vector<std::string> MapType_CComic::games() const
 {
 	std::vector<std::string> vcGames;
 	vcGames.push_back("Captain Comic");
 	return vcGames;
 }
 
-MapType::Certainty MapType_CComic::isInstance(stream::input_sptr psMap) const
+MapType::Certainty MapType_CComic::isInstance(stream::input& content) const
 {
-	stream::pos lenMap = psMap->size();
+	stream::pos lenMap = content.size();
 
 	// Make sure there's enough data to read the map dimensions
 	// TESTED BY: fmt_map_ccomic_isinstance_c01
 	if (lenMap < 4) return MapType::DefinitelyNo;
 
-	psMap->seekg(0, stream::start);
+	content.seekg(0, stream::start);
 	unsigned int width, height;
-	psMap >> u16le(width) >> u16le(height);
+	content
+		>> u16le(width)
+		>> u16le(height)
+	;
 
 	// Make sure the dimensions cover the entire file
 	// TESTED BY: fmt_map_ccomic_isinstance_c02
@@ -113,9 +268,9 @@ MapType::Certainty MapType_CComic::isInstance(stream::input_sptr psMap) const
 	if (lenMap != mapLen + 4) return MapType::DefinitelyNo;
 
 	// Read in the map and make sure all the tile codes are within range
-	uint8_t *bg = new uint8_t[mapLen];
-	boost::scoped_array<uint8_t> scoped_bg(bg);
-	stream::len r = psMap->try_read(bg, mapLen);
+	std::vector<uint8_t> bg;
+	bg.resize(mapLen);
+	stream::len r = content.try_read(bg.data(), mapLen);
 	if (r != mapLen) return MapType::DefinitelyNo; // read error
 	for (unsigned int i = 0; i < mapLen; i++) {
 		// Make sure each tile is within range
@@ -129,111 +284,32 @@ MapType::Certainty MapType_CComic::isInstance(stream::input_sptr psMap) const
 	return MapType::DefinitelyYes;
 }
 
-MapPtr MapType_CComic::create(SuppData& suppData) const
+std::unique_ptr<Map> MapType_CComic::create(
+	std::unique_ptr<stream::inout> content, SuppData& suppData) const
 {
 	// TODO: Implement
 	throw stream::error("Not implemented yet!");
 }
 
-MapPtr MapType_CComic::open(stream::input_sptr input, SuppData& suppData) const
+std::unique_ptr<Map> MapType_CComic::open(
+	std::unique_ptr<stream::inout> content, SuppData& suppData) const
 {
-	input->seekg(0, stream::start);
-	unsigned int width, height;
-	input >> u16le(width) >> u16le(height);
-	unsigned int mapLen = width * height;
-
-	// Read the background layer
-	uint8_t *bg = new uint8_t[mapLen];
-	boost::scoped_array<uint8_t> scoped_bg(bg);
-	input->read((char *)bg, mapLen);
-
-	Map2D::Layer::ItemPtrVectorPtr tiles(new Map2D::Layer::ItemPtrVector());
-	tiles->reserve(mapLen);
-	for (unsigned int i = 0; i < mapLen; i++) {
-		// The default tile actually has an image, so don't exclude it
-		//if (bg[i] == CC_DEFAULT_BGTILE) continue;
-
-		Map2D::Layer::ItemPtr t(new Map2D::Layer::Item());
-		t->type = Map2D::Layer::Item::Default;
-		t->x = i % width;
-		t->y = i / width;
-		t->code = bg[i];
-		tiles->push_back(t);
+	auto exe = suppData.find(SuppItem::Extra1);
+	if (exe == suppData.end()) {
+		throw stream::error("Missing content for layer: Extra1");
 	}
-
-	// Populate the list of permitted tiles
-	Map2D::Layer::ItemPtrVectorPtr validBGItems(new Map2D::Layer::ItemPtrVector());
-	for (unsigned int i = 0; i <= CC_MAX_VALID_TILECODE; i++) {
-		// The default tile actually has an image, so don't exclude it
-		//if (i == CC_DEFAULT_BGTILE) continue;
-
-		Map2D::Layer::ItemPtr t(new Map2D::Layer::Item());
-		t->type = Map2D::Layer::Item::Default;
-		t->x = 0;
-		t->y = 0;
-		t->code = i;
-		validBGItems->push_back(t);
-	}
-
-	// Create the map structures
-	Map2D::LayerPtr bgLayer(new Layer_CComicBackground(tiles, validBGItems));
-
-	Map2D::LayerPtrVector layers;
-	layers.push_back(bgLayer);
-
-	Map2DPtr map(new GenericMap2D(
-		Map::Attributes(), Map::GraphicsFilenames(),
-		Map2D::HasViewport,
-		193, 160, // viewport size
-		width, height,
-		CC_TILE_WIDTH, CC_TILE_HEIGHT,
-		layers, Map2D::PathPtrVectorPtr()
-	));
-
-	return map;
+	return std::make_unique<Map_CComic>(std::move(content), std::move(exe->second));
 }
 
-void MapType_CComic::write(MapPtr map, stream::expanding_output_sptr output,
-	ExpandingSuppData& suppData) const
-{
-	Map2DPtr map2d = boost::dynamic_pointer_cast<Map2D>(map);
-	if (!map2d) throw stream::error("Cannot write this type of map as this format.");
-	if (map2d->getLayerCount() != 1)
-		throw stream::error("Incorrect layer count for this format.");
-
-	unsigned int mapWidth, mapHeight;
-	map2d->getMapSize(&mapWidth, &mapHeight);
-
-	Map2D::LayerPtr layer = map2d->getLayer(0);
-
-	// Write the background layer
-	output << u16le(mapWidth) << u16le(mapHeight);
-	unsigned int mapLen = mapWidth * mapHeight;
-
-	uint8_t *bg = new uint8_t[mapLen];
-	boost::scoped_array<uint8_t> scoped_bg(bg);
-	memset(bg, CC_DEFAULT_BGTILE, mapLen); // default background tile
-	const Map2D::Layer::ItemPtrVectorPtr items = layer->getAllItems();
-	for (Map2D::Layer::ItemPtrVector::const_iterator i = items->begin();
-		i != items->end();
-		i++
-	) {
-		if (((*i)->x > mapWidth) || ((*i)->y > mapHeight)) {
-			throw stream::error("Layer has tiles outside map boundary!");
-		}
-		bg[(*i)->y * mapWidth + (*i)->x] = (*i)->code;
-	}
-
-	output->write((char *)bg, mapLen);
-	output->flush();
-	return;
-}
-
-SuppFilenames MapType_CComic::getRequiredSupps(stream::input_sptr input,
+SuppFilenames MapType_CComic::getRequiredSupps(stream::input& content,
 	const std::string& filename) const
 {
-	SuppFilenames supps;
-	return supps;
+	return {
+		std::make_pair(
+			SuppItem::Extra1,
+			"comic.exe"
+		),
+	};
 }
 
 } // namespace gamemaps
