e8ce5b65df6803fa1ad3b2838c1b2bf039cf8db1
Adam Nielsen <malvineous@shikadi.net>
2016-05-08 21:26:51 +1000

    Nukem2: Add attribute tests

--- tests/test-map-nukem2.cpp
+++ tests/test-map-nukem2.cpp
@@ -34,6 +34,73 @@ class test_map_nukem2: public test_map2d
 			this->mapCode[1].code = 0x70;
 			this->mapCode[2].pos = {0, 0};
 			this->mapCode[2].code = 0x02;
+
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Filename;
+				a.filenameValue = "czone1.mni";
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Filename;
+				a.filenameValue = "drop1.mni";
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Filename;
+				a.filenameValue = "demosong.imf";
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Enum;
+				a.enumValue = 0;
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Enum;
+				a.enumValue = 0;
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Enum;
+				a.enumValue = 0;
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Enum;
+				a.enumValue = 1;
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Integer;
+				a.integerValue = 2;
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Filename;
+				a.filenameValue = "attrfile.mni";
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Filename;
+				a.filenameValue = "tile.mni";
+			}
+			{
+				this->attributes.emplace_back();
+				auto& a = this->attributes.back();
+				a.type = Attribute::Type::Filename;
+				a.filenameValue = "maskfile.mni";
+			}
 		}
 
 		void addTests()
@@ -142,6 +209,260 @@ class test_map_nukem2: public test_map2d
 				"attrfile.mni\0"
 				"tile.mni\0\0\0\0\0"
 			));
+
+			// Attribute 00: CZone
+			this->changeAttribute(0, "test.mni", STRING_WITH_NULLS(
+				"\x35\x00"
+				"test.mni    \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x01\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 01: Backdrop
+			this->changeAttribute(1, "test.mni", STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"test.mni    \0"
+				"demosong.imf\0"
+				"\x01\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 02: Music
+			this->changeAttribute(2, "test.imf", STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"test.imf    \0"
+				"\x01\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 03: Alt backdrop
+			this->changeAttribute(3, 1, STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x41\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 04: Quake
+			this->changeAttribute(4, 1, STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x21\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 05: Backdrop movement
+			this->changeAttribute(5, 2, STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x11\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 06: Parallax
+			this->changeAttribute(6, 2, STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x02\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 07: Alt backdrop index
+			this->changeAttribute(7, 10, STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x01\x0A\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 08: Zoneattr
+			this->changeAttribute(8, "test.mni", STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x01\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"test.mni\0\0\0\0\0"
+				"tile.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 09: Zonetset
+			this->changeAttribute(9, "test.mni", STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x01\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"test.mni\0\0\0\0\0"
+				"maskfile.mni\0"
+			));
+
+			// Attribute 10: Zonemset
+			this->changeAttribute(10, "test.mni", STRING_WITH_NULLS(
+				"\x35\x00"
+				"czone1.mni  \0"
+				"drop1.mni   \0"
+				"demosong.imf\0"
+				"\x01\x02\x00\x00"
+				"\x03\x00" /* Actor ints */
+				"\x02\x00\x00\x00\x00\x00"
+				"\x40\x00" /* Map width */
+				"\x01\xC0\x10\x00\x40\x1F\x20\x00\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+				"\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0\x01\xC0"
+			) + std::string((16 * 2 + 64 * 510 + 46) * 2, '\x00') + STRING_WITH_NULLS(
+				"\x0B\x00" /* Extra length */
+				"\xFF\x03\x02\x01\xFE\x23\x45\x03\x67"
+				"\x00\x00"
+				"attrfile.mni\0"
+				"tile.mni\0\0\0\0\0"
+				"test.mni\0\0\0\0\0"
+			));
+
 		}
 
 		virtual std::string initialstate()
